de393e6c32049f538742aa7635fe3d40c093cf8a
Dan Zajdband <dan.zajdband@gmail.com>
2011-09-04 16:02:10 -0300

    up

--- chat.js
+++ chat.js
@@ -3,8 +3,6 @@ sio = require('socket.io'),
 redis = require("redis"),
 client = redis.createClient();
 
-var rooms = 0;
-
 var app = express.createServer(
     express.bodyParser()
 );
@@ -14,18 +12,21 @@ app.configure(function(){
 });
 
 app.get('/',function(req,res,next){
-	client.smembers('rooms');
-  res.render('index');
+	client.lrange(['rooms',0,-1],function(err,rooms){
+		res.locals({'rooms':rooms});
+	  res.render('index');
+	});
 });
 
 app.post('/create',function(req,res,next){
-	client.set(rooms,req.body.room_name);
-	res.redirect('/room/'+rooms);
-	rooms++;
+	client.rpush('rooms',req.body.room_name);
+	client.llen('rooms',function(err,len){
+		res.redirect('/room/'+(len - 1));
+	});
 });
 
 app.get('/room/:id',function(req,res,next){
-	client.get(req.params.id,function(err,room_name){
+	client.lindex(['rooms',req.params.id],function(err,room_name){
 		res.locals({'room_name':room_name,'room_id':req.params.id});
 		res.render('room');
 	});
@@ -37,17 +38,28 @@ io.sockets.on('connection', function (socket) {
 	socket.on('set nickname',function(data){
 		socket.join(data.room_id);
     socket.set('nickname', data.nickname, function () {
+	    socket.set('room_id', data.room_id, function () {
+				client.sadd('users'+data.room_id,data.nickname);
+	 	 	socket.broadcast.to(data.room_id).emit('new user',{'nickname':data.nickname});
+				client.smembers('users'+data.room_id,function(error,usrs){
+					socket.emit('ready',{'user_list':usrs});
+				});
+			});
     });
-		client.sadd('users'+data.room_id,data.nickname);
-  	socket.broadcast.to(data.room_id).emit('new user',{'nickname':data.nickname});
-		client.smembers('users'+data.room_id,function(error,usrs){
-			socket.emit('ready',{'user_list':usrs});
-		});
 	});
 
 	socket.on('my msg',function(data){
 		socket.get('nickname',function(err,nickname){
-	  	io.sockets.in(data.room_id).emit('new msg',{'nickname':nickname,'msg':data.msg});		
+			socket.get('room_id',function(err,room_id){			
+	 		 	io.sockets.in(room_id).emit('new msg',{'nickname':nickname,'msg':data.msg});		
+			});
+		});
+	});
+
+	socket.on('disconnect',function(){
+		socket.get('nickname',function(err,nickname){
+			client.srem('users'+data.room_id,nickname);
+	  	io.sockets.in(data.room_id).emit('user leave',{'nickname': nickname});		
 		});
 	});
 
@@ -55,7 +67,6 @@ io.sockets.on('connection', function (socket) {
 
 app.listen(3000, function (err) {
   if (err) throw err;
-
   console.log('listening on http://localhost:3000');
 });
 
