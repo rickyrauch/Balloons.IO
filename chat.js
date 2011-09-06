ca8d74318701e7354a23558f91bb2fff94aeee73
Dan Zajdband <dan.zajdband@gmail.com>
2011-09-06 13:28:10 -0300

    fixes

--- chat.js
+++ chat.js
@@ -1,15 +1,14 @@
 var express = require('express'),
 sys = require('sys'),
 sio = require('socket.io'),
-auth= require('connect-auth'),
+easyoauth = require('easy-oauth'),
 redis = require("redis"),
 client = redis.createClient();
 
-var consumer_key = 's6iO5CDeqZiJrpgnivltQ',
-	consumer_secret = 'gqRlE3dG0m7svYWSkuIlTd0hZCO3JN5Bk4eYch5KE';
 
-var access_token= '308711490-aGd70sodkgzAJ4xrRephF5myVXuDT025vF7RIwau';
-var access_token_secret= 'vlzu0NUCzrWXUtpYnLX1o1hqFJQ3aOKH8cGUQDtsCs';
+
+//var access_token= '308711490-aGd70sodkgzAJ4xrRephF5myVXuDT025vF7RIwau';
+//var access_token_secret= 'vlzu0NUCzrWXUtpYnLX1o1hqFJQ3aOKH8cGUQDtsCs';
 
 
 
@@ -20,11 +19,11 @@ var app = express.createServer(
 app.configure(function(){
   app.set('view engine', 'jade');  
   app.use(express.static(__dirname + '/public'));
+  app.use(express.bodyParser());
 	app.use(express.cookieParser());
 	app.use(express.session({ secret: 'foobar' }));
-	app.use(auth( [
-		auth.Twitter({consumerKey: consumer_key, consumerSecret: consumer_secret})
-  ]));
+  app.use(easyoauth(require('./keys_file')));
+  app.use(app.router);
 });
 
 app.get('/',function(req,res,next){
@@ -41,11 +40,17 @@ app.post('/create',function(req,res,next){
 	});
 });
 
-app.get('/room/:id',protect,function(req,res,next){
-	client.lindex(['rooms',req.params.id],function(err,room_name){
-		res.locals({'room_name':room_name,'room_id':req.params.id});
-		res.render('room');
-	});
+app.get('/room/:id',function(req,res,next){
+	if(req.getAuthDetails().user){
+		client.lindex(['rooms',req.params.id],function(err,room_name){
+			client.smembers('users'+req.params.id,function(error,user_list){
+					res.locals({'room_name':room_name,'room_id':req.params.id,'username': req.getAuthDetails().user.username,'user_list':user_list});
+					res.render('room');
+			});
+		});
+	}
+	else
+		res.redirect('back');
 });
 
 var io = sio.listen(app);
@@ -56,10 +61,7 @@ io.sockets.on('connection', function (socket) {
 	   socket.set('nickname', data.nickname, function () {
 	    socket.set('room_id', data.room_id, function () {
 				client.sadd('users'+data.room_id,data.nickname);
-	 	 	socket.broadcast.to(data.room_id).emit('new user',{'nickname':data.nickname});
-				client.smembers('users'+data.room_id,function(error,usrs){
-					socket.emit('ready',{'user_list':usrs});
-				});
+	 	 		socket.broadcast.to(data.room_id).emit('new user',{'nickname':data.nickname});
 			});
 		});
 	});
@@ -87,19 +89,3 @@ app.listen(3000, function (err) {
   console.log('listening on http://localhost:3000');
 });
 
-
-function protect(req, res, next) {
-  if( req.isAuthenticated() ) next();
-  else {
-    req.authenticate(function(error, authenticated) {
-      if( error ) next(new Error("Problem authenticating"));
-      else {
-        if( authenticated === true)next();
-        else if( authenticated === false ) next(new Error("Access Denied!"));
-        else {
-          // Abort processing, browser interaction was required (and has happened/is happening)
-        }
-      }
-    })
-  }
-}
