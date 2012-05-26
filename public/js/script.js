74d16b8be30ef8feb891bbd61e5f6af04f198391
Cristian Douce <cristian@gravityonmars.com>
2012-05-25 23:39:43 -0300

    indentations from 4 to 2 spaces

--- public/js/script.js
+++ public/js/script.js
@@ -1,175 +1,175 @@
 $(function() {
-	var USERS = window.USERS = {};
+  var USERS = window.USERS = {};
 
-	$('.people a').each(function(index, element) {
-		USERS[$(element).data('username')] = 1;
-	});
+  $('.people a').each(function(index, element) {
+    USERS[$(element).data('username')] = 1;
+  });
 
-	//View handlers
-	$(".dropdown a.selected").click(function() {
-		$('.create-room').show().next(".text").hide();
-		$(this).toggleClass("active");
-		$(this).next(".dropdown-options").toggle();
-	});
-	
-	$(".create-room").click(function() {
-		$(this).hide();
-		$(this).next(".text").fadeIn();
-	});
-	
-	$(".lock").click(function() {
-		$(this).toggleClass('active');
-	});
-	
-	$(".fancybox").fancybox({'margin': 0, 'padding': 0});
-	
-	$(".invite-people").click(function(){
-		$(this).hide().after('<p class="inviting-people">Inviting peple, please wait.</p>').delay(2000).hide().after('something');
-	});
+  //View handlers
+  $(".dropdown a.selected").click(function() {
+    $('.create-room').show().next(".text").hide();
+    $(this).toggleClass("active");
+    $(this).next(".dropdown-options").toggle();
+  });
+  
+  $(".create-room").click(function() {
+    $(this).hide();
+    $(this).next(".text").fadeIn();
+  });
+  
+  $(".lock").click(function() {
+    $(this).toggleClass('active');
+  });
+  
+  $(".fancybox").fancybox({'margin': 0, 'padding': 0});
+  
+  $(".invite-people").click(function(){
+    $(this).hide().after('<p class="inviting-people">Inviting peple, please wait.</p>').delay(2000).hide().after('something');
+  });
 
-	//Socket.io
-	var socket = io.connect('http://localhost');
+  //Socket.io
+  var socket = io.connect('http://localhost');
 
-	socket.emit('set nickname', {
-		room_id: $('#room_id').text(),
-		nickname: $('#username').text()
-	});
+  socket.emit('set nickname', {
+    room_id: $('#room_id').text(),
+    nickname: $('#username').text()
+  });
 
-	socket.on('ready', function(data) {
-		//If chat is empty, request for history!
-		if($('.chat .chat-box').length == 0) {
-			socket.emit('history request');
-		}
-	});
+  socket.on('ready', function(data) {
+    //If chat is empty, request for history!
+    if($('.chat .chat-box').length == 0) {
+      socket.emit('history request');
+    }
+  });
 
-	socket.on('history response', function(data) {
-		if(data.history && data.history.length) {
-			data.history.reverse()
-			data.history.forEach(function(historyLine) {
-				var time = new Date(historyLine.atTime),
-					chatBoxData = {
-						nickname: historyLine.from,
-						msg: historyLine.withData,
-						time: time,
-						tt: time.getHours() > 12 ? "PM" : "AM"
-					};
+  socket.on('history response', function(data) {
+    if(data.history && data.history.length) {
+      data.history.reverse()
+      data.history.forEach(function(historyLine) {
+        var time = new Date(historyLine.atTime),
+          chatBoxData = {
+            nickname: historyLine.from,
+            msg: historyLine.withData,
+            time: time,
+            tt: time.getHours() > 12 ? "PM" : "AM"
+          };
 
-				$('.chat').prepend(ich.chat_box(chatBoxData));
-				$('.chat').scrollTop($('.chat').height());
-			});
-		}
-	});
+        $('.chat').prepend(ich.chat_box(chatBoxData));
+        $('.chat').scrollTop($('.chat').height());
+      });
+    }
+  });
 
-	socket.on('new user', function(data) {
-		var message = "$username has joined the room.";
+  socket.on('new user', function(data) {
+    var message = "$username has joined the room.";
 
-		if(!USERS[data.nickname]) {
-			$('.online .people').prepend(ich.people_box(data));
-			USERS[data.nickname] = 1;
+    if(!USERS[data.nickname]) {
+      $('.online .people').prepend(ich.people_box(data));
+      USERS[data.nickname] = 1;
 
-			// Chat notice
-			message = message
-						.replace('$username', data.nickname);
+      // Chat notice
+      message = message
+            .replace('$username', data.nickname);
 
-			// Check update time
-			var time = new Date(),
-				noticeBoxData = {
-					noticeMsg: message,
-					time: time,
-					tt: time.getHours() > 12 ? "PM" : "AM"
-				};
+      // Check update time
+      var time = new Date(),
+        noticeBoxData = {
+          noticeMsg: message,
+          time: time,
+          tt: time.getHours() > 12 ? "PM" : "AM"
+        };
 
-			$('.chat').append(ich.chat_notice(noticeBoxData));
-			$('.chat').scrollTop($('.chat').height());
-		}
-	});
+      $('.chat').append(ich.chat_notice(noticeBoxData));
+      $('.chat').scrollTop($('.chat').height());
+    }
+  });
 
-	socket.on('user-info update', function(data) {
-		var message = "$username is now $status.";
+  socket.on('user-info update', function(data) {
+    var message = "$username is now $status.";
 
-		// Update dropdown
-		if(data.username == $('#username').text()) {
-			$('.dropdown-status .list a').toggleClass('current', false);
-			$('.dropdown-status .list a.' + data.status).toggleClass('current', true);
+    // Update dropdown
+    if(data.username == $('#username').text()) {
+      $('.dropdown-status .list a').toggleClass('current', false);
+      $('.dropdown-status .list a.' + data.status).toggleClass('current', true);
 
-			$('.dropdown-status a.selected')
-				.removeClass('available away busy');
+      $('.dropdown-status a.selected')
+        .removeClass('available away busy');
 
-			$('.dropdown-status a.selected').addClass(data.status).html('<b></b>' + data.status);
-		}
+      $('.dropdown-status a.selected').addClass(data.status).html('<b></b>' + data.status);
+    }
 
-		// Update users list
-		$('.people a[data-username=' + data.username + ']')
-			.removeClass('available away busy')
-			.addClass(data.status);
+    // Update users list
+    $('.people a[data-username=' + data.username + ']')
+      .removeClass('available away busy')
+      .addClass(data.status);
 
-		// Chat notice
-		message = message
-					.replace('$username', data.username)
-					.replace('$status', data.status);
+    // Chat notice
+    message = message
+          .replace('$username', data.username)
+          .replace('$status', data.status);
 
-		// Check update time
-		var time = new Date(),
-			noticeBoxData = {
-				noticeMsg: message,
-				time: time,
-				tt: time.getHours() > 12 ? "PM" : "AM"
-			};
+    // Check update time
+    var time = new Date(),
+      noticeBoxData = {
+        noticeMsg: message,
+        time: time,
+        tt: time.getHours() > 12 ? "PM" : "AM"
+      };
 
-		$('.chat').append(ich.chat_notice(noticeBoxData));
-		$('.chat').scrollTop($('.chat').height());
-	});
+    $('.chat').append(ich.chat_notice(noticeBoxData));
+    $('.chat').scrollTop($('.chat').height());
+  });
 
-	socket.on('new msg', function(data) {
-		var time = new Date();
+  socket.on('new msg', function(data) {
+    var time = new Date();
 
-		data.time = time;
-		data.tt = time.getHours() > 12 ? "PM" : "AM";
+    data.time = time;
+    data.tt = time.getHours() > 12 ? "PM" : "AM";
 
-		$('.chat').append(ich.chat_box(data));
-		$('.chat').scrollTop($('.chat').height());
-	});
+    $('.chat').append(ich.chat_box(data));
+    $('.chat').scrollTop($('.chat').height());
+  });
 
-	socket.on('user leave', function(data) {
-		var nickname = $('#username').text(),
-			message = "$username has left the room.";
-		
-		$('.people a').each(function(index, element) {
-			var checkUsername = $(element).data('username');
+  socket.on('user leave', function(data) {
+    var nickname = $('#username').text(),
+      message = "$username has left the room.";
+    
+    $('.people a').each(function(index, element) {
+      var checkUsername = $(element).data('username');
 
-			if(checkUsername == data.nickname) {
-				if (checkUsername != nickname ) {
-					$(element).remove();
-					USERS[data.nickname] = 0;
+      if(checkUsername == data.nickname) {
+        if (checkUsername != nickname ) {
+          $(element).remove();
+          USERS[data.nickname] = 0;
 
-					// Chat notice
-					message = message
-								.replace('$username', data.nickname);
+          // Chat notice
+          message = message
+                .replace('$username', data.nickname);
 
-					$('.chat').append(ich.chat_notice({
-						noticeMsg: message
-					})).scrollTop($('.chat').height());
-				};
-			}
-		});
-	});
+          $('.chat').append(ich.chat_notice({
+            noticeMsg: message
+          })).scrollTop($('.chat').height());
+        };
+      }
+    });
+  });
 
-	$(".chat-input input").keypress(function(e) {
-		if(e.which == 13) {
-			socket.emit('my msg', {
-				msg: $(this).val(),
-				room_id: $('#room_id').text()
-			});
+  $(".chat-input input").keypress(function(e) {
+    if(e.which == 13) {
+      socket.emit('my msg', {
+        msg: $(this).val(),
+        room_id: $('#room_id').text()
+      });
 
-			$(this).val('');
+      $(this).val('');
 
-			return false;
-		}
-	});
+      return false;
+    }
+  });
 
-	$('.dropdown-status .list a').click(function(e) {
-		socket.emit('set status', {
-			status: $(this).data('status')
-		});
-	})
+  $('.dropdown-status .list a').click(function(e) {
+    socket.emit('set status', {
+      status: $(this).data('status')
+    });
+  })
 });
