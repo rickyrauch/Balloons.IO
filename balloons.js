9c511a9ad24aca15ccfac505470f4472a5e10f98
Dan Zajdband <dan.zajdband@gmail.com>
2012-07-24 16:44:58 -0300

    moved routes to another file

--- balloons.js
+++ balloons.js
@@ -20,7 +20,7 @@ var express = require('express')
  * Instantiate redis
  */
 
-var client = redis.createClient();
+var client = exports.client  = redis.createClient();
 
 /*
  * Clean db and create folder
@@ -38,7 +38,7 @@ require('./strategy');
  * Create and config server
  */
 
-var app = express.createServer();
+var app = exports.app = express.createServer();
 
 app.configure(function() {
   app.set('view engine', 'jade'); 
@@ -60,77 +60,7 @@ app.configure(function() {
  * Routes
  */
 
-/*
- * Homepage
- */
-
-app.get('/', function(req, res, next) {
-  if(req.isAuthenticated()){
-    client.hmset(
-        'users:' + req.user.username
-      , req.user
-    );
-    res.redirect('/rooms/list');
-  } else{
-    res.render('index');
-  }
-});
-
-/*
- * Authentication routes
- */
-
-app.get('/auth/twitter', passport.authenticate('twitter'));
-
-app.get('/auth/twitter/callback', 
-  passport.authenticate('twitter', { failureRedirect: '/' }),
-  function(req, res) {
-    res.redirect('/');
-});
-
-app.get('/logout', function(req, res){
-  req.logout();
-  res.redirect('/');
-});
-
-/*
- * Rooms list
- */
-
-app.get('/rooms/list', utils.restrict, function(req, res) {
-  utils.getPublicRoomsInfo(client, function(rooms) {
-    res.render('room_list', { rooms: rooms });
-  });
-});
-
-/*
- * Create a rooom
- */
-
-app.post('/create', utils.restrict, function(req, res) {
-  utils.validRoomName(req, res, function(roomKey) {
-    utils.roomExists(req, res, client, roomKey, function() {
-      utils.createRoom(req, res, client, roomKey);
-    });
-  });
-});
-
-/*
- * Join a room
- */
-
-app.get('/rooms/:id', utils.restrict, function(req, res) {
-  utils.getRoomInfo(req, res, client, function(room) {
-    utils.getUsersInRoom(req, res, client, room, function(users) {
-      utils.getPublicRoomsInfo(client, function(rooms) {
-        utils.getUserStatus(req.user.username, client, function(status) {
-          utils.enterRoom(req, res, room, users, rooms, status);
-        });
-      });
-    });
-  });
-});
-
+require('./routes');
 
 /*
  * Socket.io
---
+++ routes/index.js
@@ -0,0 +1,81 @@
+
+/*
+ * Module dependencies
+ */
+
+var app = module.parent.exports.app
+  , passport = require('passport')
+  , client = module.parent.exports.client
+  , utils = require('../utils');
+
+/*
+ * Homepage
+ */
+
+app.get('/', function(req, res, next) {
+  if(req.isAuthenticated()){
+    client.hmset(
+        'users:' + req.user.username
+      , req.user
+    );
+    res.redirect('/rooms/list');
+  } else{
+    res.render('index');
+  }
+});
+
+/*
+ * Authentication routes
+ */
+
+app.get('/auth/twitter', passport.authenticate('twitter'));
+
+app.get('/auth/twitter/callback', 
+  passport.authenticate('twitter', { failureRedirect: '/' }),
+  function(req, res) {
+    res.redirect('/');
+});
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+/*
+ * Rooms list
+ */
+
+app.get('/rooms/list', utils.restrict, function(req, res) {
+  utils.getPublicRoomsInfo(client, function(rooms) {
+    res.render('room_list', { rooms: rooms });
+  });
+});
+
+/*
+ * Create a rooom
+ */
+
+app.post('/create', utils.restrict, function(req, res) {
+  utils.validRoomName(req, res, function(roomKey) {
+    utils.roomExists(req, res, client, roomKey, function() {
+      utils.createRoom(req, res, client, roomKey);
+    });
+  });
+});
+
+/*
+ * Join a room
+ */
+
+app.get('/rooms/:id', utils.restrict, function(req, res) {
+  utils.getRoomInfo(req, res, client, function(room) {
+    utils.getUsersInRoom(req, res, client, room, function(users) {
+      utils.getPublicRoomsInfo(client, function(rooms) {
+        utils.getUserStatus(req.user.username, client, function(status) {
+          utils.enterRoom(req, res, room, users, rooms, status);
+        });
+      });
+    });
+  });
+});
+
