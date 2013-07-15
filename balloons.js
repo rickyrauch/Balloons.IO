b1254852ddf27b6c460a7ce471346e30047043dc
Cristian Douce <cristian@gravityonmars.com>
2013-07-15 02:09:32 -0300

    Update booting

--- balloons.js
+++ balloons.js
@@ -3,37 +3,7 @@
  */
 
 var express = require('express')
-  , passport = require('passport')
-  , config = require('./config.json')
   , init = require('./init')
-  , redis = require('redis')
-  , RedisStore = require('connect-redis')(express);
-
-/*
- * Instantiate redis
- */
-
-if (process.env.REDISTOGO_URL) {
-  var rtg   = require('url').parse(process.env.REDISTOGO_URL);
-  var client = exports.client  = redis.createClient(rtg.port, rtg.hostname);
-  client.auth(rtg.auth.split(':')[1]); // auth 1st part is username and 2nd is password separated by ":"
-} else {
-  var client = exports.client  = redis.createClient();
-}
-
-var sessionStore = exports.sessionStore = new RedisStore({client: client});
-
-/*
- * Clean db and create folder
- */
-
-init(client);
-
-/*
- * Passportjs auth strategy
- */
-
-require('./strategy');
 
 /*
  * Create and config server
@@ -41,35 +11,38 @@ require('./strategy');
 
 var app = exports.app = express();
 
-app.configure(function() {
-  app.set('port', process.env.PORT || config.app.port || 6789);
-  app.set('view engine', 'jade'); 
-  app.set('views', __dirname + '/views/themes/' + config.theme.name);
-  app.use(express.static(__dirname + '/public'));
-  app.use(express.bodyParser());
-  app.use(express.cookieParser(config.session.secret));
-  app.use(express.session({
-    key: "balloons",
-    store: sessionStore
-  }));
-  app.use(passport.initialize());
-  app.use(passport.session());
-  app.use(app.router);
-});
+/**
+ * Configure application
+ */
+
+require('./config')(app);
+
+/*
+ * Clean db and create folder
+ */
+
+// init(app.get('redisClient'));
+
+/*
+ * Passportjs auth strategy
+ */
+
+require('./strategy')(app);
+
 
 /*
  * Routes
  */
 
-require('./routes');
+require('./routes')(app);
 
 /*
  * Web server
  */
 
-if(config.credentials) {
+if(app.get('config').credentials) {
   exports.server = require('https')
-  .createServer(config.credentials, app).listen(app.get('port'), function() {
+  .createServer(app.get('config').credentials, app).listen(app.get('port'), function() {
     console.log('Balloons.io started on port %d', app.get('port'));
   });
 } else {
@@ -83,7 +56,7 @@ if(config.credentials) {
  * Socket.io
  */
 
-require('./sockets');
+require('./sockets')(app, exports.server);
 
 
 /*
