195126195b8e2decfe80debd2439d201944e235c
senorcris <cristian.taveras@nytimes.com>
2014-03-28 23:25:28 -0400

    adding authentication for using github keys

--- strategy.js
+++ strategy.js
@@ -5,7 +5,8 @@
 
 var passport = require('passport')
   , TwitterStrategy = require('passport-twitter').Strategy
-  , FacebookStrategy = require('passport-facebook').Strategy 
+  , FacebookStrategy = require('passport-facebook').Strategy
+  , GitHubStrategy = require('passport-github').Strategy;
 
 /**
  * Expose Authentication Strategy
@@ -51,6 +52,18 @@ function Strategy (app) {
         callbackURL: config.auth.facebook.callback
       },
       function(accessToken, refreshToken, profile, done) {
+        return done(null, profile);
+      }
+    ));
+  }
+
+  if(config.auth.github.clientid.length) {
+    passport.use(new GitHubStrategy({
+        clientID: config.auth.github.clientid,
+        clientSecret: config.auth.github.clientsecret,
+        callbackURL: config.auth.github.callback
+      },
+      function(token, tokenSecret, profile, done) {
         return done(null, profile);
       }
     ));
